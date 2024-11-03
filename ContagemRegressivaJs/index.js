f03463f2aeb3ef981354fdd9b1e781641ad29925
RuanMSantos <ruancauancauan@gmail.com>
2024-11-03 19:52:35 -0300

    Finaliza exercício

---
+++ ContagemRegressivaJs/index.js
@@ -0,0 +1,13 @@
+setTimeout(P, 1000);
+setTimeout(A, 1000);
+setTimeout(F, 1000);
+
+function P() {
+    alert("Preparar...");
+}
+function A(){
+    alert("Apontar...");
+}
+function F(){
+    alert("FOGO...");
+}
